ada1a31a39be47ddadb784d9c2aafb0d529e87da
Hank McGill <henrymcgill@gmail.com>
2023-10-11 18:13:01 -0400

    adding components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import Login from "./components/Login";
 import Profile from "./components/Profile";
 
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-import { Amplify, Auth } from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
 
 // Configure Amplify in index file or root file
@@ -13,12 +12,18 @@ Amplify.configure(awsExports)
 export default function App() {
   return (
     <Authenticator loginMechanisms={['username','email']}>
-      {({ signOut, user }) => (
+      {({ signOut, user }) => {
+
+        console.log('user', user)
+
+        return (
         <main>
           <h1>Hello {user.username}</h1>
+          <Profile />
           <button onClick={signOut}>Sign out</button>
         </main>
-      )}
+        )
+      }}
     </Authenticator>
   );
 }
