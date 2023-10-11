21cf4819429624a63e4ec7fdfa326598f2e3ed13
Hank McGill <henrymcgill@gmail.com>
2023-10-10 20:04:40 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 function App() {
   return (
-    <div>
-     hello there!
-    </div>
+      <Authenticator>
+          {({ signOut, user }) => (
+              <div>
+                  <p>Welcome {user.username}</p>
+                  <button onClick={signOut}>Sign out</button>
+              </div>
+          )}
+      </Authenticator>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+
+// Configure Amplify in index file or root file
+Amplify.configure({
+  Auth: {
+      region: awsExports.REGION,
+      userPoolId: awsExports.USER_POOL_ID,
+      userPoolWebClientId: awsExports.USER_POOL_APP_CLIENT_ID
+  }
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
