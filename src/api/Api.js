2111ca5da7fda19386e64759f02388276cf02c7c
Hank McGill <henrymcgill@gmail.com>
2023-10-18 15:57:18 -0400

    updated post req works on postman, not in app yet

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,5 +1,32 @@
 export async function getTest () {
-  await fetch('https://api.github.com/users/xiaotian/repos').then(
+  await fetch('https://gad6zvq0pc.execute-api.us-east-1.amazonaws.com/DynamoDB_API/DynamoDBManager',
+  {
+    method: 'POST',
+    body: JSON.stringify(
+      {
+        "operation": "update",
+        "payload": {
+            "Key": {
+                "id": "1234ABCD"
+            },
+            "AttributeUpdates": {
+                "number": {
+                    "Value": 66
+                }
+            }
+        }
+      }
+      ),
+    // mode: 'no-cors',
+    headers: {
+    //   'Content-Type': 'application/json',
+    //   'Access-Control-Allow-Methods':'*',
+    //   'Access-Control-Allow-Origin' : '*',
+    //   'Access-Control-Allow-Headers':'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+    //   'Access-Control-Allow-Credentials' : true,
+    },
+  }
+  ).then(
     resp => resp.json() // this returns a promise
   ).then(repos => {
     for (const repo of repos) {
