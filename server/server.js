e03fb34e5f21ca47885730a8d0dc38e1a43c0363
Hank McGill <henrymcgill@gmail.com>
2023-10-28 15:35:09 -0400

    testing updates to site

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
-const path = require('path')
 
 app.use(express.static("build"));
 
 app.use('/test', (req, res) => {
-    res.send("this is a new test")
-  })
+  res.send("this is an even newer test")
+})
 
 app.listen(PORT, console.log(`listening on port: ${PORT}`))
