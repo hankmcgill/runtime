9f7571dc546fe7b640bfac9871792789b6d1c62b
hankmcgill <henrymcgill@gmail.com>
2023-10-29 17:38:59 -0400

    final test push

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import Header from "./components/Header"
-import Profile from "./components/Profile";
-import Footer from "./components/Footer"
+import Header from './components/Header'
+import Profile from './components/Profile'
+import Footer from './components/Footer'
 
-export default function App() {  
+export default function App() {
   return (
     <body>
       <Header />
-      oh, hello from my new mac!
+      oh, hello from my new mac! this is the final test push.
       <Profile />
       <Footer />
     </body>
-  );
+  )
 }
