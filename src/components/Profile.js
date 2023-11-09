6cc83edb5f4ec10297af2dcf31514f91448e99eb
hankmcgill <henrymcgill@gmail.com>
2023-11-09 11:13:37 -0500

    began implemtenting front end queries to back end

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,5 @@
-// import { Auth } from 'aws-amplify'
-// ;(await Auth.currentSession()).getIdToken().getJwtToken()
-
 export default function Profile(props) {
-  console.log('user:', props.user)
+  console.log('user:', props.user.attributes)
 
   return <main>{/* <h1>Hello {props.user.username}</h1> */}</main>
 }
